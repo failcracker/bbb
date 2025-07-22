dcde9cbac18aef79da226c65659ba0c1e554ca47
failcracker <87586254+failcracker@users.noreply.github.com>
2025-07-23 05:53:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import auth, users, absensi, admin
 import uvicorn
+
+# List origins (domain) yang diizinkan
+origins = [
+    "https://frontend-mobilefacenet.vercel.app",
+    "http://localhost",  # Tambahkan ini untuk development lokal
+    "http://localhost:3000", # Contoh jika FE Anda berjalan di port 3000
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Mengizinkan semua metode (GET, POST, PUT, dll.)
+    allow_headers=["*"],  # Mengizinkan semua header
+)
 
 app = FastAPI(title="Absensi Face Recognition API")
 
